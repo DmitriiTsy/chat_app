b5c3cb9b8a5a1286203a441f4e27c2ad504123a1
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2024-09-01 16:08:13 -0700

    delete_redux_for_now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 import Chat from './components/Chat'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
+
 
 export default function Home() {
   return (
-    <Provider store={store}>
       <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-500">
         <Chat />
       </main>
-    </Provider>
   )
 }
